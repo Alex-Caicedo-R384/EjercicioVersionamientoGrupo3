4b801dd9fff9c45acbfd21d329c7545f6065f315
Alex_Caicedo_R384 <131833190+Alex-Caicedo-R384@users.noreply.github.com>
2025-06-23 03:10:19 +0000

    Añade logotipo ASCII para versión v1.1.0

--- src/main/java/com/ejemplo/Main.java
+++ src/main/java/com/ejemplo/Main.java
@@ -2,7 +2,10 @@ package com.ejemplo;
 
 public class Main {
     public static void main(String[] args) {
-        Despedida despedida = new Despedida();
+        System.out.println("*********************************************");
+        System.out.println("* ¨PROYECTO DE JAVA PARA EL NADA HARO *");
+        System.out.println("*********************************************");
+        Despedida despedida = new Despedida();        
         System.out.println(despedida.generarDespedida("Nada Haro"));
     }
 }
